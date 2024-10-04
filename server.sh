abc06953d397af5e1ebe2d306cb38f004405df1e
Joe McCormick <joe.c.mccormick@gmail.com>
2024-10-04 07:32:16 -0700

    simplify build

--- server.sh
+++ server.sh
@@ -5,4 +5,4 @@
 # sudo hugo serve --liveReloadPort 443 --port=443 --appendPort=false --baseURL=$(grep APP_HOST_URL .env | cut -d '=' -f2) -s . -d ./public
 
 # with local
-hugo serve -s . -d ./build
+hugo serve
